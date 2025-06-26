b8abcbb6ccd10145a2fad044788dec7b3a632c6c
Atul kumar jha <jhaak301@gmail.com>
2025-06-26 17:02:59 +0530

    Third commit

---
+++ src/app/api/auth/callback/route.ts
@@ -0,0 +1,43 @@
+import { NextRequest, NextResponse } from 'next/server';
+
+const client_id = process.env.SPOTIFY_CLIENT_ID!;
+const client_secret = process.env.SPOTIFY_CLIENT_SECRET!;
+const redirect_uri = process.env.SPOTIFY_REDIRECT_URI!;
+
+
+export async function GET(req: Nextrequest) {
+    const code = req.nextUrl.searchParams.get('code');
+
+    if(!code) {
+        return NextResponse.json({ error: 'Authorization code missing'}, { status: 400 });
+    }
+
+    const tokenResponse = await fetch('https://accounts.spotify.com/api/token', {
+        method: 'POST',
+        headers: {
+            'Authorization': 'Basic ' +Buffer.from(`${client_id}:${client_secret}`).toString('base64'),
+            'Content-Type': 'application/x-www-form-urlencoded',
+        },
+        body: new URLSearchParams({
+            grant_type: 'authorization_code',
+            code,
+            redirect_uri,
+        }),
+    });
+
+    const data = await tokenResponse.json();
+
+    if(data.error) {
+        return NextResponse.json({ error: data.error_desccriptioon || 'token excahnge failed' }, { status: 400 });
+    }
+
+    const { access_token, refresh_token, expires_in } = data;
+
+    const redirectUrl = new URL(req.nextUrl.origin);
+    redirectUrl.pathname = '/dashboard';
+    redirectUrl.searchParams.set('access_token', access_token);;
+    redirectUrl.searchParams.set('refresh_token', refresh_token);
+    redirectUrl.searchParams.set('expires_in', expires_in.toString());
+
+    return NextResponse.redirect(redirectUrl.toString());
+}
---
+++ src/app/api/auth/route.ts
@@ -0,0 +1,20 @@
+import { NextResponse } from 'next/server';
+
+const client_id = process.env.SPOTIFY_CLIENT_ID!;
+const redirect_uri = process.env.SPOTIFY_REDIRECT_URL!;
+const scopes = [
+    'user-read-private',
+    'user-read-email',
+    'playlist-modify-public',
+    'playlist-modify-private'
+];
+
+export async function GET() {
+    const authUrl = new URL("https://accounts.spotify.com/authorize");
+    authUrl.searchParams.set('response_type', 'code');
+    authUrl.searchParams.set('client_id',client_id);
+    authUrl.searchParams.set('scope',scopes.join(' '));
+     authUrl.searchParams.set('redirect_uri',redirect_uri);
+     
+     return NextResponse.redirect(authUrl.toString());
+}
